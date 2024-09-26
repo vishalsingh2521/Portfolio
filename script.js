b7cd8d5b7c6c98530a9d5fe978bdcd1059d88b1b
unknown <vishalsingh.2531@gmail.com>
2024-09-27 02:34:09 +0530

    new update

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
+//counter
 const counters = document.querySelectorAll('.value');
 const speed = 1000;
 
 const animateCounters = (counter) => {
-  const updateCounter = () => {
-    const value = +counter.getAttribute('num');
-    const data = +counter.innerText;
-    counter.innerText = data < value ? Math.ceil(data + value / speed) : value;
-    if (data < value) setTimeout(updateCounter, 20);
-  };
-  updateCounter();
+    const updateCounter = () => {
+        const value = +counter.getAttribute('num');
+        const data = +counter.innerText;
+        counter.innerText = data < value ? Math.ceil(data + value / speed) : value;
+        if (data < value) setTimeout(updateCounter, 20);
+    };
+    updateCounter();
 };
 
 const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      animateCounters(entry.target);
-      observer.unobserve(entry.target); // Stop observing after the animation starts
-    }
-  });
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            animateCounters(entry.target);
+            observer.unobserve(entry.target); // Stop observing after the animation starts
+        }
+    });
 }, {
-  root: null, // Use the viewport as the root
-  threshold: 0.5 // Trigger when 50% of the container is visible
+    root: null, // Use the viewport as the root
+    threshold: 0.5 // Trigger when 50% of the container is visible
 });
 
 // Start observing each counter
 counters.forEach(counter => observer.observe(counter));
+
+
 
 // hover code for the services section
 const serviceItems = document.querySelectorAll('.service-dis');
